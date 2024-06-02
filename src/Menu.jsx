a8d16409b7b2efedab93ee5b157caeb07b8b06dd
EstebanPederiva <estebanpederiva05@gmail.com>
2024-06-02 20:41:53 -0300

    Entrega-final

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Image from 'react-bootstrap/Image';
-import logo from './assets/logo.jpg';
-import CartWidget from './CartWidget';
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Image from 'react-bootstrap/Image'
+import logo from './assets/logo.jpg'
+import CartWidget from './CartWidget'
 
 function menu() {
   return (
